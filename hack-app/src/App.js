cd235d3ae02e24b69e4240c4ab94bd679dad24e5
andreeapetrascu <andreea.petrascu.e@gmail.com>
2022-10-01 15:29:24 +0300

    solution

--- hack-app/src/App.js
+++ hack-app/src/App.js
@@ -1,16 +1,48 @@
-import React from 'react';
-// import Map from './components/Map';
+import React, {useEffect, useState} from 'react';
+import Map from './components/Map';
 import './App.css';
+const axios = require("axios");
 
-// TODO: start here
+
+
 
 function App() {
+    const {response} = axios.get('http://localhost:3001');
+
   return (
     <>
       <h1>Haufe Hackathon</h1>
-      {/* <Map/> */}
+      {<Map/> }
+
+        <table>
+            <h1>Puncte de colectare</h1>
+            <tbody>
+            <tr>
+                <th>_id</th>
+                <th>id</th>
+                <th>tip colecatre</th>
+                <th>adresa</th>
+                <th>companie</th>
+                <th>website</th>
+                <th>latitudine</th>
+                <th>longitudine</th>
+            </tr>
+            {/*{response.map((item, i) => (*/}
+            {/*    <tr key={i}>*/}
+            {/*        <td>{item.result.records._id}</td>*/}
+            {/*        <td>{item.result.records.id}</td>*/}
+            {/*        <td>{item.result.records['tip colectare']}</td>*/}
+            {/*        <td>{item.result.records.adresa}</td>*/}
+            {/*        <td>{item.result.records.companie}</td>*/}
+            {/*        <td>{item.result.records.website}</td>*/}
+            {/*        <td>{item.result.records.latitudine}</td>*/}
+            {/*        <td>{item.result.records.longitudine}</td>*/}
+            {/*    </tr>*/}
+            {/*))}*/}
+            </tbody>
+        </table>
+
     </>
   );
 }
-
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,1 +1,22 @@
-// TODO: start here
+const express = require('express')
+const axios = require("axios");
+const {response} = require("express");
+const app = express()
+const port = 3001
+var cors = require('cors')
+
+app.use(cors())
+
+app.get('/', async (req, res) => {
+
+    const data = await axios.get('https://data.primariatm.ro/api/3/action/datastore_search?resource_id=d0134630-84d9-40b8-9bcb-dfdc926d66ab')
+
+    console.log(data);
+    return data;
+    //res.send(data)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
